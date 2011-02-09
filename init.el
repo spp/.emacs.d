80c05b793b94986361ca5aefb970a76b7f22e08d
Habibullah Pagarkar <habibpagarkar@gmail.com>
2011-02-09 22:30:49 +0530

    Adding more options to Ido mode. Using my version of rvm.el

--- init.el
+++ init.el
@@ -59,6 +59,14 @@
 ;; Startup IDo
 (require 'ido)
 (ido-mode t)
+(ido-everywhere t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-max-prospects 10)
+(defun ido-complete-hook ()
+  (define-key ido-completion-map [tab] 'ido-complete))
+(add-hook 'ido-setup-hook 'ido-complete-hook)
 
 ;; YAML mode
 (require 'yaml-mode)
